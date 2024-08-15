13d4ff1e9977dca94744a42ab3773c5887202e5b
Malanaa <abdullahdotpy@gmail.com>
2024-08-15 17:02:54 -0500

    error handling git fix

--- database/mongoclient.py
+++ database/mongoclient.py
@@ -9,10 +9,10 @@ load_dotenv()
 mongo_connection_string = os.environ.get("CUSTOMCONNSTR_MONGO_CONNECTION_STRING")
 print(mongo_connection_string)
 
-if not mongo_connection_string or not mongo_connection_string.startswith(
-    ("mongodb://", "mongodb+srv://")
-):
-    raise ValueError("MONGO_CONNECTION_STRING is not set correctly in the environment.")
+# if not mongo_connection_string or not mongo_connection_string.startswith(
+#     ("mongodb://", "mongodb+srv://")
+# ):
+    # raise ValueError("MONGO_CONNECTION_STRING is not set correctly in the environment.")
 
 # Creating the MongoDB Client
 client = pymongo.MongoClient(
