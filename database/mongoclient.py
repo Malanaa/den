35bed4cf73da3007c57536a8ddd52213ae27d307
Malanaa <abdullahdotpy@gmail.com>
2024-08-15 14:36:19 -0500

    mongo print debug

---
+++ database/mongoclient.py
@@ -0,0 +1,21 @@
+import pymongo
+import certifi
+from datetime import date
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+mongo_connection_string = os.getenv("MONGO_CONNECTION_STRING")
+print(mongo_connection_string)
+
+if not mongo_connection_string or not mongo_connection_string.startswith(
+    ("mongodb://", "mongodb+srv://")
+):
+    raise ValueError("MONGO_CONNECTION_STRING is not set correctly in the environment.")
+
+# Creating the MongoDB Client
+client = pymongo.MongoClient(
+    mongo_connection_string,
+    tlsCAFile=certifi.where(),
+)
